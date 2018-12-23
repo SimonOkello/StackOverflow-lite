21d5675014ecadc8eac81e5b14511b26c16f41ca
SimonOkello <simonokello93@gmail.com>
2018-12-23 15:55:24 +0300

    initialize repo with a basic Html Layout

--- stack/forms.py
+++ stack/forms.py
@@ -3,15 +3,16 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, 
 
 
-class Registration(FlaskForm):
+class RegistrationForm(FlaskForm):
     username = StringField("Username", validators = [DataRequired(), Length(min = 5, max= 20)])
     email = StringField("Email", validators = [DataRequired(), Email])
-    password = PasswordField("Password", validators = [DataRequired(), Length(min = 5 max = 15)])
+    password = PasswordField("Password", validators = [DataRequired(), Length(min = 5 ,max = 15)])
     Confirm_password = PasswordField("Confirm_Password", validators = [DataRequired(), EqualTo("password")])
     submit = SubmitField("Sign Up")
 
-class Login(FlaskForm):
-    email = StringField("Email", validators = [DataRequired(), Email])
-    password = PasswordField("Password", validators = [DataRequired(), Length(min = 5 max = 15)])
+class LoginForm(FlaskForm):
+    email = StringField("Email", validators = [DataRequired()])
+    password = PasswordField("Password", validators = [DataRequired()])
+    remember = BooleanField('Remember Me')
     submit = SubmitField("Sign Up")
 
