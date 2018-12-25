0089b5cac7304c8b91eaf3b5ca717f5b480e1c3a
kalyakiplangat <kalyakiplangat@gmail.com>
2018-12-25 15:03:36 +0300

    added venv

--- lite/Scripts/Activate.ps1
+++ lite/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\Users\Simon Okello\Documents\ANDELA\StackOverFlow-lite\lite"
+$env:VIRTUAL_ENV="C:\Users\Cynthy\Documents\Andel\StackOverflow-lite\lite"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
--- lite/Scripts/activate.bat
+++ lite/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\Simon Okello\Documents\ANDELA\StackOverFlow-lite\lite"
+set "VIRTUAL_ENV=C:\Users\Cynthy\Documents\Andel\StackOverflow-lite\lite"
 
 if not defined PROMPT (
     set "PROMPT=$P$G"
