31fd33c31084d5e2b44c67fd898e1e0aa8c85c94
SimonOkello <simonokello93@gmail.com>
2018-12-25 10:27:05 +0300

    More changes

--- stack/routes.py
+++ stack/routes.py
@@ -3,9 +3,17 @@ from stack import app
 #from stack.forms import RegistrationForm, LoginForm
 
 @app.route("/")
-def index():
+def home():
     return render_template("index.html")
 
 @app.route("/login")
 def login():
     return render_template("login.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
