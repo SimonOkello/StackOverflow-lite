b68da4fb37c7661506d1b38f098e08157db440b2
SimonOkello <simonokello93@gmail.com>
2018-12-25 12:44:57 +0300

    More changes

--- stack/__init__.py
+++ stack/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 
 
 
-app  =  Flask(__name__)
+app = Flask(__name__)
 
 app.config["SECRET_KEY"] = "f43f7d7a100fe149d743fc800901f7df"
-
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
+db = SQLAlchemy(app)
 from stack import routes
