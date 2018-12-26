38e1d2814b209c768b838a89ac68d3cf8837e17a
SimonOkello <simonokello93@gmail.com>
2018-12-26 11:16:36 +0300

    More changes to database file

--- stack/__init__.py
+++ stack/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 
 from stack.forms import LoginForm, RegistrationForm
@@ -8,8 +9,10 @@ app = Flask(__name__)
 
 app.config["SECRET_KEY"] = "f43f7d7a100fe149d743fc800901f7df"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from stack import routes
+from stack import routes, models
 
 
